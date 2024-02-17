677e8fb8122f52305ee105a35bf359870dea0160
Nimra-create <nimraraza985@gmail.com>
2024-02-17 08:04:47 -0800

    message

---
+++ index.js
@@ -0,0 +1,28 @@
+// function  displayWelcomeMesaage(username){
+//     alert('hello '+ username)
+// }
+// displayWelcomeMesaage("NIMRA");
+
+
+//question 2
+// const quotes = ['ESCAPE THE ORDINARY', 'LIVE WHAT YOU LOVE', 'KNOW YOUR WORTH'];
+
+// function generateQuote() {
+//     const randomIndex = Math.floor(Math.random() * quotes.length);
+//     alert(quotes[randomIndex]);
+// }
+
+// generateQuote();
+
+
+
+//question 3
+
+function generateRandomNumber(){
+    const randomIndex = Math.floor(Math.random() * 100);
+       alert([randomIndex]);
+     }
+
+     generateRandomNumber();
+
+
